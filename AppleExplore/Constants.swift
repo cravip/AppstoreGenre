306b34038bb15994b28192683239aa6a54c9f0b5
chandra <chandra.patel@kuliza.com>
2016-08-27 16:56:18 +0530

    add initial base files

---
+++ AppleExplore/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  AppleExplore
+//
+//  Created by kuliza-195 on 27/08/16.
+//  Copyright © 2016 Chandra. All rights reserved.
+//
+
+import Foundation
+
+class Constants {
+    
+    
+}
--- AppleExplore/ViewController.swift
+++ AppleExplore/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var slidingView: UIView!
+    
+    @IBOutlet weak var exploreCollectionView: UICollectionView!
+    
+    @IBOutlet weak var exploreTableView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
 }
 
+extension ViewController : UITableViewDataSource, UITableViewDelegate {
+    
+}
